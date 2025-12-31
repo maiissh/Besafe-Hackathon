aa44b13ea80187b3d3458f1bbfbb61a266d95d7b
Salma <salma.jabareen@mail.huji.ac.il>
2025-12-31 13:49:01 +0200

    Add SpotGameHomePage Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/HomePage/HomePage';
 import Register from './pages/Register/Register';
 import './pages/Register/Register.css';
@@ -20,18 +20,14 @@ function App() {
 
       <main className={styles.main}>
         <Routes>
-          {/* 1. COMMENT OUT THE REAL HOME PAGE 
-             <Route path="/" element={<Home />} /> 
-          */}
-
-          {/* 2. ADD THIS TEMPORARY LINE */}
-          {/* Now, when you open http://localhost:5173/, you will see YOUR page immediately */}
           <Route path="/" element={<SpotGameHomePage />} />
-
-          {/* Keep your other routes just in case */}
           <Route path="/game" element={<SpotGameHomePage />} />
           
           {/* ... other routes ... */}
+          <Route path="/home" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/chat-info" element={<ChatInfoPage />} />
+          <Route path="/chat" element={<ChatPage />} />
         </Routes>
       </main>
 
@@ -39,27 +35,6 @@ function App() {
     </BrowserRouter>
   );
 }
-
-  // return (
-  //   <BrowserRouter>
-  //     <div className={styles.app}>
-  //       <header className={styles.appHeader}>
-  //         <img src={projectLogo} alt="Logo" className={styles.appLogo} />
-  //         <nav className={styles.appNav}>
-  //           <Link to="/" className={styles.appLink}>Home</Link>
-  //         </nav>
-  //       </header>
-  //       <main className={styles.main}>
-  //         <Routes>
-  //           <Route path="/" element={<Home />} />
-  //         </Routes>
-  //       </main>
-  //       <footer className={styles.footer}>
-  //         <p>&copy; 2024 My App</p>
-  //       </footer>
-  //     </div>
-  //   </BrowserRouter>
-  // );
 
 
 export default App;
