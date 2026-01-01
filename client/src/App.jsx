4e2e5f7ec6d91ebd4b70823f1a71861148598745
maiissh <maisshande@gmail.com>
2026-01-01 22:48:12 +0200

    reorder the fiels and make some changes in the css & clear all the errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'; 
 import Home from './page/Homepage/HomePage';
-import Register from './page/Register/Register';
-import './page/Register/Register.css';
 import styles from './styles/App.module.css';
 import StoriesSection from './page/Stories/StoriesSection';
-import SpotGameHomePage from './page/SpotImposterGame/SpotGameHomePage';
-import ChatInfoPage from './page/ChatInfo/ChatInfoPage';
-import ChatPage from './page/Chat/ChatPage';
-
-
+import SpotGameHomePage from './page/SpotImposterGame/SpotGameHomePage';  
+import ChatInfoPage from './page/ChatInfoPage/ChatInfoPage';              
+import ChatPage from './page/ChatPage/ChatPage';                         
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className={styles.app}>
-        <main className={styles.main}>
-          <Routes>
-            <Route path="/Homepage" element={<Home />} />
-            <Route path="/" element={<Register />} /> 
-            <Route path="/Stories" element={<StoriesSection />} /> 
-            <Route path="/game" element={<SpotGameHomePage />} />
-            <Route path="/chat-info" element={<ChatInfoPage />} />
-            <Route path="/chat" element={<ChatPage />} />
-          </Routes>
-        </main>
-      </div>
-    </BrowserRouter>
+    <div className={styles.app}>
+      <main className={styles.main}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Stories" element={<StoriesSection />} /> 
+          <Route path="/game" element={<SpotGameHomePage />} />
+          <Route path="/chat-info" element={<ChatInfoPage />} />
+          <Route path="/chat" element={<ChatPage />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,11 +1,9 @@
-
 import PropTypes from 'prop-types';
 import { Flame, Coins } from "lucide-react";
-import PropTypes from 'prop-types';
 import styles from "./Header.module.css";
+
 export default function Header({  points, streak }) {
   return (
-
     <header className={styles.header}>
       <div className={styles.headerTop}>
         <div className={styles.branding}>
