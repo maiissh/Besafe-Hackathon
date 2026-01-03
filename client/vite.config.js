3b402ba970b9265bde9587d8add2307e4e688e78
maiissh <maisshande@gmail.com>
2026-01-03 21:09:07 +0200

    test done in client

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +8,11 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true // Automatically open the browser
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
   }
 })
